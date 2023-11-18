bc60413bc6f96a35b884fdaa7331a9a9be77e1b7
Matteo Piran <pmatteo@users.noreply.github.com>
2023-11-18 18:54:06 +0100

    [FEATURE] Use `amqp91` deferred confirmation (#4)

--- tests/benchmark/publisher_test.go
+++ tests/benchmark/publisher_test.go
@@ -7,14 +7,37 @@ import (
 	cmap "github.com/orcaman/concurrent-map"
 
 	fr "github.com/pmatteo/friendlyrabbit"
+	"github.com/pmatteo/friendlyrabbit/tests/mock"
 )
+
+func BenchmarkPublishWithConfirmationForDuration(b *testing.B) {
+	purgeQueue(b)
+
+	timeDuration := time.Minute * 5
+	if testing.Short() {
+		timeDuration = time.Minute * 1
+	}
+
+	b.Logf("Benchmark Starts: %s\r\n", time.Now())
+	b.Logf("Est. Benchmark End: %s\r\n", time.Now().Add(timeDuration))
+
+	b.ReportAllocs()
+
+	publisher := fr.NewPublisherFromConfig(seasoning, connectionPool)
+	defer publisher.Shutdown(false)
+
+	publishDone := make(chan bool, 1)
+	conMap := cmap.New()
+	publishWithConfirmation(b, conMap, publishDone, timeDuration, publisher)
+	<-publishDone
+}
 
 func BenchmarkPublishForDuration(b *testing.B) {
 	purgeQueue(b)
 
-	timeDuration := time.Minute * 2
+	timeDuration := time.Minute * 5
 	if testing.Short() {
-		timeDuration = time.Second * 25
+		timeDuration = time.Minute * 1
 	}
 
 	b.Logf("Benchmark Starts: %s\r\n", time.Now())
@@ -29,4 +52,45 @@ func BenchmarkPublishForDuration(b *testing.B) {
 	conMap := cmap.New()
 	publishLoop(b, conMap, publishDone, timeDuration, publisher)
 	<-publishDone
+}
+
+func publishLoop(
+	b *testing.B,
+	conMap cmap.ConcurrentMap,
+	done chan bool,
+	timeoutDuration time.Duration,
+	publisher *fr.Publisher,
+) {
+	messagesPublished := 0
+	messagesFailedToPublish := 0
+	publisherErrors := 0
+
+	timeout := time.After(timeoutDuration)
+
+PublishLoop:
+	for {
+		select {
+		case <-timeout:
+			break PublishLoop
+		default:
+			newLetter := mock.CreateMockRandomWrappedBodyLetter("TestBenchmarkQueue")
+			conMap.Set(newLetter.LetterID.String(), false)
+			publisher.Publish(newLetter, false)
+
+			notice := <-publisher.PublishReceipts()
+			if notice.Success {
+				messagesPublished++
+				notice = nil
+			} else {
+				messagesFailedToPublish++
+				notice = nil
+			}
+		}
+	}
+
+	b.Logf("Publisher Errors: %d\r\n", publisherErrors)
+	b.Logf("Messages Published: %d\r\n", messagesPublished)
+	b.Logf("Messages Failed to Publish: %d\r\n", messagesFailedToPublish)
+
+	done <- true
 }
