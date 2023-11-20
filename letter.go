eccb3457b5e731a8a42a296a1c35ef4f4988f3ca
pmatteo <piranmatteo@gmail.com>
2023-11-20 18:46:37 +0100

    feat: add New* for Envelope and Letter

--- letter.go
+++ letter.go
@@ -47,18 +47,57 @@ type ModdedBody struct {
 	Data        []byte `json:"Data"`
 }
 
-// CreateLetter creates a simple letter for publishing.
-func CreateLetter(exchangeName string, routingKey string, body []byte) *Letter {
+func NewEnvelope(
+	ctx context.Context,
+	exchangeName string,
+	routingKey string,
+	headers amqp.Table,
+) *Envelope {
+	return &Envelope{
+		Ctx:          context.Background(),
+		Exchange:     exchangeName,
+		RoutingKey:   routingKey,
+		ContentType:  "application/json",
+		Mandatory:    false,
+		Immediate:    false,
+		DeliveryMode: amqp.Persistent,
+		Headers:      headers,
+	}
+}
 
-	envelope := &Envelope{
-		Ctx:         context.Background(),
-		Exchange:    exchangeName,
-		RoutingKey:  routingKey,
-		ContentType: "application/json",
+func NewLetterWithPayload(
+	envelope *Envelope,
+	data interface{},
+	encryption *EncryptionConfig,
+	compression *CompressionConfig,
+	wrapPayload bool,
+	metadata string,
+) (*Letter, error) {
+	var letterID = uuid.New()
+
+	if wrapPayload {
+		body, err := CreateWrappedPayload(data, letterID, metadata, compression, encryption)
+		if err != nil {
+			return nil, err
+		}
+		return NewLetter(letterID, envelope, body), nil
 	}
 
+	body, err := CreatePayload(data, compression, encryption)
+	if err != nil {
+		return nil, err
+	}
+	return NewLetter(letterID, envelope, body), nil
+}
+
+// CreateLetter creates a simple letter for publishing.
+func NewLetter(
+	letterID uuid.UUID,
+	envelope *Envelope,
+	body []byte,
+) *Letter {
 	return &Letter{
-		LetterID:   uuid.New(),
+		LetterID:   letterID,
 		RetryCount: uint32(3),
 		Body:       body,
 		Envelope:   envelope,
